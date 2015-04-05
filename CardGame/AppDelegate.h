4b4af316f9ce73214443b71f42a8d3f7d82d2187
rookie <rookie@userdeMacBook-Pro-2.local>
2015-04-05 15:19:53 +0300

    Initial Commit

---
+++ CardGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardGame
+//
+//  Created by rookie on 05/04/15.
+//  Copyright (c) 2015 rookie studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CardGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CardGame
+//
+//  Created by rookie on 05/04/15.
+//  Copyright (c) 2015 rookie studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
